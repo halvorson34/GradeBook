b637ad09bf6e56a0fb2728a7848690eceea1868a
Matt Halvorson <halvorson34.mh@gmail.com>
2021-05-13 16:53:02 -0500

    create list variable with foreach loop and find average grade with write to console

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,20 +10,13 @@ namespace GradeBook
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
-            int x = 1;
-            int y = 2;
-            int result = x + y;
-
-            System.Console.WriteLine(result);
-
-            var numbers = new[] { 1, 2, 3, 4 };
-
-            var result2 = 0;
-            foreach (var number in numbers)
+            var result = 0.0;
+            foreach (var number in grades)
             {
-                result2 += number;
+                result += number;
             }
-            System.Console.WriteLine(result2);
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N1}");
 
             if (args.Length > 0)
             {
