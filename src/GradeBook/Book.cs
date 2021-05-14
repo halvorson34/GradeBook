b32ce2450e1fa5a727357a88bab72585732fcd60
Matt Halvorson <halvorson34.mh@gmail.com>
2021-05-14 14:50:29 -0500

    move math comps to showstat method in Book.cs class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -5,15 +6,35 @@ namespace GradeBook
 
     class Book
     {
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
 
-        List<double> grades;
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
     }
 }
