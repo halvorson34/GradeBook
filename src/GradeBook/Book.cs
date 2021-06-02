73cd4da53be97637eae40ed6ec48208e49dd45ec
Matt Halvorson <halvorson34.mh@gmail.com>
2021-06-02 12:29:57 -0500

    add exception to AddGrade method in Book.cs

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -41,7 +41,7 @@ namespace GradeBook
             }
             else
             {
-                Console.WriteLine("Invalid value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
 
